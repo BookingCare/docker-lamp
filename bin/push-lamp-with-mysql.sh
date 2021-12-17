d3c0f737767b2db9b89c47b603daa17fb15f7425
Alex Pham <alex@gotitapp.co>
2021-12-17 23:08:16 +0700

    chore: add scripts to push images

---
+++ bin/push-lamp-with-mysql.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+cd ..
+
+VERSION=1.0.0
+docker build -f Dockerfile-with-mysql -t lamp-with-mysql:"$VERSION" .
+docker tag lamp-with-mysql:"$VERSION" registry.gitlab.com/bookingcare/bookingcare-2016/lamp-with-mysql:"$VERSION"
+docker push registry.gitlab.com/bookingcare/bookingcare-2016/lamp-with-mysql:"$VERSION"
+
+docker image prune -f
---
+++ bin/push-lamp-without-mysql.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+cd ..
+
+VERSION=1.0.0
+docker build -f Dockerfile-without-mysql -t lamp-without-mysql:"$VERSION" .
+docker tag lamp-without-mysql:"$VERSION" registry.gitlab.com/bookingcare/bookingcare-2016/lamp-without-mysql:"$VERSION"
+docker push registry.gitlab.com/bookingcare/bookingcare-2016/lamp-without-mysql:"$VERSION"
+
+docker image prune -f
