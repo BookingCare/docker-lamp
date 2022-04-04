5f5e21f8af64018a48a04159f33e2058abba6461
Alex Pham <>
2022-04-04 09:03:41 +0700

    feat: minor updates

--- bin/push-lamp-with-mysql.sh
+++ bin/push-lamp-with-mysql.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 cd ..
 
-VERSION=1.0.0
+VERSION=1.1.0
 docker build -f Dockerfile-with-mysql -t lamp-with-mysql:"$VERSION" .
 docker tag lamp-with-mysql:"$VERSION" registry.gitlab.com/bookingcare/bookingcare-2016/lamp-with-mysql:"$VERSION"
 docker push registry.gitlab.com/bookingcare/bookingcare-2016/lamp-with-mysql:"$VERSION"
--- bin/push-lamp-without-mysql.sh
+++ bin/push-lamp-without-mysql.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 cd ..
 
-VERSION=1.0.0
+VERSION=1.2.0
 docker build -f Dockerfile-without-mysql -t lamp-without-mysql:"$VERSION" .
 docker tag lamp-without-mysql:"$VERSION" registry.gitlab.com/bookingcare/bookingcare-2016/lamp-without-mysql:"$VERSION"
 docker push registry.gitlab.com/bookingcare/bookingcare-2016/lamp-without-mysql:"$VERSION"
