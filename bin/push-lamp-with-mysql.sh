90d6c3398679149415ba9a613b596590d00cab31
Alex Pham <>
2022-04-05 10:52:58 +0700

    chore: update registy url

--- bin/push-lamp-with-mysql.sh
+++ bin/push-lamp-with-mysql.sh
@@ -5,7 +5,7 @@ cd ..
 
 VERSION=1.1.0
 docker build -f Dockerfile-with-mysql -t lamp-with-mysql:"$VERSION" .
-docker tag lamp-with-mysql:"$VERSION" registry.gitlab.com/bookingcare/bookingcare-2016/lamp-with-mysql:"$VERSION"
-docker push registry.gitlab.com/bookingcare/bookingcare-2016/lamp-with-mysql:"$VERSION"
+docker tag lamp-with-mysql:"$VERSION" registry.gitlab.com/bookingcare/lamp/lamp-with-mysql:"$VERSION"
+docker push registry.gitlab.com/bookingcare/lamp/lamp-with-mysql:"$VERSION"
 
 docker image prune -f
--- bin/push-lamp-without-mysql.sh
+++ bin/push-lamp-without-mysql.sh
@@ -5,7 +5,7 @@ cd ..
 
 VERSION=1.2.0
 docker build -f Dockerfile-without-mysql -t lamp-without-mysql:"$VERSION" .
-docker tag lamp-without-mysql:"$VERSION" registry.gitlab.com/bookingcare/bookingcare-2016/lamp-without-mysql:"$VERSION"
-docker push registry.gitlab.com/bookingcare/bookingcare-2016/lamp-without-mysql:"$VERSION"
+docker tag lamp-without-mysql:"$VERSION" registry.gitlab.com/bookingcare/lamp/lamp-without-mysql:"$VERSION"
+docker push registry.gitlab.com/bookingcare/lamp/lamp-without-mysql:"$VERSION"
 
 docker image prune -f
