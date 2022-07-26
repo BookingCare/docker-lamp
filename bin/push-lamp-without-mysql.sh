81c5b16787ca6021b41f1cc28f7ec6debf5a3781
Alex Pham <>
2022-07-26 09:53:28 +0700

    feat: add php-intl

--- bin/push-lamp-without-mysql.sh
+++ bin/push-lamp-without-mysql.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 cd ..
 
-VERSION=1.2.0
+VERSION=1.3.0
 docker build -f Dockerfile-without-mysql -t lamp-without-mysql:"$VERSION" .
 docker tag lamp-without-mysql:"$VERSION" registry.gitlab.com/bookingcare/lamp/lamp-without-mysql:"$VERSION"
 docker push registry.gitlab.com/bookingcare/lamp/lamp-without-mysql:"$VERSION"
